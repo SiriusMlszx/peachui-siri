fb3d36302d3a5b4716a52ea356c1c2ea9ef83597
Sirius <83930223@qq.com>
2021-03-02 17:12:54 +0800

    topNav组件封装

--- src/views/Dock.vue
+++ src/views/Dock.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="topNav">
-    <div class="logo"></div>
-    <div class="menu"></div>
-  </div>
-  <div class="content">
-    <aside>边栏</aside>
-    <main>主内容</main>
+  <div>
+    <Topnav/>
+    <div class="content">
+      <aside>边栏</aside>
+      <main>主内容</main>
+    </div>
   </div>
 </template>
 <!--<style lang="scss" scoped>-->
 
 <!--</style>-->
+<script>
+import Topnav from "../components/Topnav.vue"
+export default {
+  components: {Topnav}
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
 <div>
-  <div class="topNav">
-    <div class="logo">LOGO</div>
-    <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
-    </ul>
-  </div>
+  <Topnav/>
   <div class="banner">
     <h1>SiriUI</h1>
     <h2>一个厉害的UI框架</h2>
@@ -19,21 +13,11 @@
 </template>
 
 <style lang="scss" scoped>
-.topNav{
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo{
-    min-width: 6em;
-    margin-right: auto;
-  }
-  > .menu{
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
+
 </style>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue"
+export default {
+  components: {Topnav}
+}
+</script>
