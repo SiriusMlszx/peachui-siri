7becb88a1d4b7e9ec3b0db0e696fe9cf955e6b71
Sirius <83930223@qq.com>
2021-03-03 12:40:35 +0800

    可以接受value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>switch文档</p>
+  <div>
+    <Switch :value="y" @input="y=$event"/>
+  </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+export default {
+  components:{Switch},
+  setup(){
+    const y = ref(false)
+    return {y}
+  }
+}
+</script>
