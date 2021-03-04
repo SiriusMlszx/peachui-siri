706b43ab3683c0c2bd592aa02087d3f96c84bf07
Sirius <83930223@qq.com>
2021-03-04 22:26:27 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="siri-tabs">
-    <div class="siri-tabs-nav">
-      <div class="siri-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t===selected}" :key="index">
+    <div class="siri-tabs-nav" ref="container">
+      <div class="siri-tabs-nav-item" v-for="(t,index) in titles"
+           :ref="el => {if (el) navItems[index] = el}"
+           @click="select(t)"
+           :class="{selected: t===selected}"
+           :key="index">
         {{ t }}
       </div>
+      <div class="siri-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="siri-tabs-content">
       <component class="siri-tabs-content-item" :class="{selected: c.props.title === selected}" v-for="c in defaults" :is="c"/>
@@ -13,7 +18,7 @@
 
   <script lang="ts">
 import Tab from "./Tab.vue";
-import {computed} from "vue";
+import {computed,ref,onMounted,onUpdated} from "vue";
 
 export default {
   props: {
@@ -22,6 +27,21 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div=>div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {left: left1} = container.value.getBoundingClientRect()
+      const {left: left2} = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -39,7 +59,7 @@ export default {
     const select = (title: string)=>{
       context.emit('update:selected', title)
     }
-    return {defaults, titles, select, current};
+    return {defaults, titles, select, current, navItems,indicator, container};
   }
 };
 </script>
@@ -53,6 +73,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -63,6 +84,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator{
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
