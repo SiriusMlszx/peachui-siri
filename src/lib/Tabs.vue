2d40d7fc2c247347f3d8d24a0a159c749cd2a57b
Sirius <83930223@qq.com>
2021-03-04 19:27:58 +0800

    使用CSS切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,79 @@
 <template>
-<div></div>
+  <div class="siri-tabs">
+    <div class="siri-tabs-nav">
+      <div class="siri-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t===selected}" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="siri-tabs-content">
+      <component class="siri-tabs-content-item" :class="{selected: c.props.title === selected}" v-for="c in defaults" :is="c"/>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+  <script lang="ts">
+import Tab from "./Tab.vue";
+import {computed} from "vue";
+
 export default {
-name: "Tabs"
-}
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是Tab");
+      }
+    });
+    const current = computed(()=>{
+      return defaults.filter((tag)=>{
+        return tag.props.title === props.selected
+      })[0]
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: string)=>{
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, select, current};
+  }
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.siri-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 
 </style>
