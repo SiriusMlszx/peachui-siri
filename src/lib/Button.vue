27d7e0c5b20c44a11e163c0e259ae30e3c4abd5e
Sirius <83930223@qq.com>
2021-03-04 12:17:19 +0800

    支持disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="siri-button" :class="classes">
+  <button class="siri-button" :class="classes" :disabled="disabled">
     <slot/>
   </button>
 </template>
@@ -15,15 +15,24 @@ export default {
     size: {
       type: String,
       default: "normal",
+    },
+    level:{
+      type: String,
+      default: "normal",
+    },
+    disabled:{
+      type: Boolean,
+      default: false
     }
   },
   setup(props) {
-    const {theme, size} = props
+    const {theme, size, level} = props
     const classes = computed(() => {
 
       return {
         [`siri-theme-${theme}`]: theme,
         [`siri-size-${size}`]: size,
+        [`siri-level-${level}`]: level,
       };
     });
     return {classes}
@@ -37,6 +46,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey:grey;
 .siri-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +62,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -75,7 +87,8 @@ $radius: 4px;
     box-shadow: none;
     color: $blue;
 
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%)
     }
   }
@@ -85,20 +98,83 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
 
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       background: darken(white, 5%);
     }
   }
+  &.siri-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.siri-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
   &.siri-theme-button{
-    &.siri-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px;
+    &.siri-level-main{
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus{
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.siri-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.siri-level-danger{
+      background: $red;
+      border-color: $red;
+      color:white;
+      &:hover,
+      &:focus{
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.siri-theme-link{
+    &.siri-level-danger{
+      color: $red;
+      &:hover,
+      &:focus{
+        color: darken($red, 10%)
+      }
+    }
+  }
+  &.siri-theme-text{
+    &.siri-level-main{
+      color: $blue;
+      &:hover,
+      &:focus{
+        color: darken($blue, 10%)
+      }
+    }
+    &.siri-level-danger{
+      color: $red;
+      &:hover,
+      &:focus{
+        color: darken($red, 10%)
+      }
+    }
+  }
+  &.siri-theme-button{
+    &[disabled]{
+      cursor: not-allowed;
+      color: grey;
+      &:hover{
+        border-color: $grey;
+      }
+    }
+  }
+  &.siri-theme-link, &.siri-theme-text{
+    &[disaboed]{
+      cursor: not-allowed;
+      color: $grey;
     }
   }
 }
