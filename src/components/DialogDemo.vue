552b4e6901148aeac08c42ca168d16f95acae82f
Sirius <83930223@qq.com>
2021-03-04 14:36:11 +0800

    创建Tabs和Tab组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -10,27 +10,43 @@
         <strong>hi</strong>
       </template>
     </Dialog>
+    <h1>示例2</h1>
+    <Button @click="showDialog">show</Button>
   </div>
 </template>
 <script lang="ts">
 
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
-import {ref} from 'vue'
+import {ref} from "vue";
+import {openDialog} from "../lib/openDialog";
+
 export default {
   components: {Button, Dialog},
-  setup(){
-    const x = ref(false)
+  setup() {
+    const x = ref(false);
     const toggle = () => {
-      x.value = !x.value
-    }
-    const f1 = () =>{
-      return false
-    }
-    const f2=()=>{
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {
 
-    }
-    return{x,toggle,f1,f2}
+    };
+    const showDialog = () => {
+      openDialog({
+        title: "标题",
+        content: "你好",
+        ok(){
+          console.log("ok");
+        },
+        cancel(){
+          console.log("cancel");
+        }
+      });
+    };
+    return {x, toggle, f1, f2, showDialog};
   }
-}
+};
 </script>
