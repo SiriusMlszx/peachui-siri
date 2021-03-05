7193030e6f339ae38e6defc92f489f417c1f92ba
Sirius <83930223@qq.com>
2021-03-05 12:16:55 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,7 +2,7 @@
   <div class="siri-tabs">
     <div class="siri-tabs-nav" ref="container">
       <div class="siri-tabs-nav-item" v-for="(t,index) in titles"
-           :ref="el => {if (el) navItems[index] = el}"
+           :ref="el => {if (t === selected) selectedItem = el}"
            @click="select(t)"
            :class="{selected: t===selected}"
            :key="index">
@@ -17,52 +17,45 @@
 </template>
 
   <script lang="ts">
-import Tab from "./Tab.vue";
-import {computed,ref,onMounted,onUpdated} from "vue";
+  import Tab from "./Tab.vue";
+  import {ref, onMounted, onUpdated} from "vue";
 
-export default {
-  props: {
-    selected: {
-      type: String
-    }
-  },
-  setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
-    const indicator = ref<HTMLDivElement>(null)
-    const container = ref<HTMLDivElement>(null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div=>div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {left: left1} = container.value.getBoundingClientRect()
-      const {left: left2} = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
-    const defaults = context.slots.default();
-    defaults.forEach((tag) => {
-      if (tag.type !== Tab) {
-        throw new Error("Tabs 子标签必须是Tab");
+  export default {
+    props: {
+      selected: {
+        type: String
       }
-    });
-    const current = computed(()=>{
-      return defaults.filter((tag)=>{
-        return tag.props.title === props.selected
-      })[0]
-    })
-    const titles = defaults.map((tag) => {
-      return tag.props.title;
-    });
-    const select = (title: string)=>{
-      context.emit('update:selected', title)
+    },
+    setup(props, context) {
+      const selectedItem = ref<HTMLDivElement>(null);
+      const indicator = ref<HTMLDivElement>(null);
+      const container = ref<HTMLDivElement>(null);
+      const x = () => {
+        const {width} = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const {left: left1} = container.value.getBoundingClientRect();
+        const {left: left2} = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      };
+      onMounted(x);
+      onUpdated(x);
+      const defaults = context.slots.default();
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+          throw new Error("Tabs 子标签必须是Tab");
+        }
+      });
+      const titles = defaults.map((tag) => {
+        return tag.props.title;
+      });
+      const select = (title: string) => {
+        context.emit("update:selected", title);
+      };
+      return {defaults, titles, select, selectedItem, indicator, container};
     }
-    return {defaults, titles, select, current, navItems,indicator, container};
-  }
-};
-</script>
+  };
+  </script>
 
 <style lang="scss">
 $blue: #40a9ff;
